9d24c6225d1eea84f376668dc04fbbcb8d4b441a
Yoel <ygonzalez.work@gmail.com>
2021-05-23 00:15:14 -0300

    add: 404 page

---
+++ src/pages/commons/404.js
@@ -0,0 +1,16 @@
+import React from "react"
+import {Container} from "reactstrap"
+import Breadcrumb from "../../components/Common/Breadcrumb";
+
+const PageNotFount = () => {
+    return (
+        <React.Fragment>
+            <div className="page-content">
+                <Container fluid>
+                    <h1>404</h1>
+                </Container>
+            </div>
+        </React.Fragment>
+    )
+}
+export default PageNotFount;
