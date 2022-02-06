b91224f275c093209c0329e21b73d3e54ad67725
Yoel <ygonzalez.work@gmail.com>
2022-02-05 21:31:12 -0300

    busquedas generales por productos y clientes (usando prefijos)

--- src/components/HorizontalLayout/Header.js
+++ src/components/HorizontalLayout/Header.js
@@ -1,17 +1,15 @@
-import React, { useState } from "react"
+import React, {useState} from "react"
 import PropTypes from 'prop-types'
 
-import { connect } from "react-redux"
+import {connect} from "react-redux"
 
-import {BrowserRouter as Router, Link} from "react-router-dom"
+import {Link} from "react-router-dom"
 
 // Redux Store
-import { showRightSidebarAction, toggleLeftmenu } from "../../store/actions"
+import {showRightSidebarAction, toggleLeftmenu} from "../../store/actions"
 // reactstrap
-import { Row, Col, Dropdown, DropdownToggle, DropdownMenu } from "reactstrap"
 
 // Import menuDropdown
-import LanguageDropdown from "../CommonForBoth/TopbarDropdown/LanguageDropdown"
 import NotificationDropdown from "../CommonForBoth/TopbarDropdown/NotificationDropdown"
 import ProfileMenu from "../CommonForBoth/TopbarDropdown/ProfileMenu"
 
@@ -20,136 +18,123 @@ import logoLight from "../../assets/images/logo-light.png"
 import logoDark from "../../assets/images/logo-dark.png"
 
 // import images
-import github from "../../assets/images/brands/github.png"
-import bitbucket from "../../assets/images/brands/bitbucket.png"
-import dribbble from "../../assets/images/brands/dribbble.png"
-import dropbox from "../../assets/images/brands/dropbox.png"
-import mail_chimp from "../../assets/images/brands/mail_chimp.png"
-import slack from "../../assets/images/brands/slack.png"
 
 //i18n
-import { withTranslation } from "react-i18next"
+import {withTranslation} from "react-i18next"
 import UsersSalesDropdown from "../CommonForBoth/TopbarDropdown/UsersSalesDropdown";
+import GeneralSearch from "../CommonForBoth/TopbarDropdown/GeneralSearch";
 
 const Header = props => {
-  const [isSearch, setSearch] = useState(false)
-  const [socialDrp, setsocialDrp] = useState(false)
+    const [isSearch, setSearch] = useState(false)
 
-  function toggleFullscreen() {
-    if (
-      !document.fullscreenElement &&
-      /* alternative standard method */ !document.mozFullScreenElement &&
-      !document.webkitFullscreenElement
-    ) {
-      // current working methods
-      if (document.documentElement.requestFullscreen) {
-        document.documentElement.requestFullscreen()
-      } else if (document.documentElement.mozRequestFullScreen) {
-        document.documentElement.mozRequestFullScreen()
-      } else if (document.documentElement.webkitRequestFullscreen) {
-        document.documentElement.webkitRequestFullscreen(
-          Element.ALLOW_KEYBOARD_INPUT
-        )
-      }
-    } else {
-      if (document.cancelFullScreen) {
-        document.cancelFullScreen()
-      } else if (document.mozCancelFullScreen) {
-        document.mozCancelFullScreen()
-      } else if (document.webkitCancelFullScreen) {
-        document.webkitCancelFullScreen()
-      }
+
+    function toggleFullscreen() {
+        if (
+            !document.fullscreenElement &&
+            /* alternative standard method */ !document.mozFullScreenElement &&
+            !document.webkitFullscreenElement
+        ) {
+            // current working methods
+            if (document.documentElement.requestFullscreen) {
+                document.documentElement.requestFullscreen()
+            } else if (document.documentElement.mozRequestFullScreen) {
+                document.documentElement.mozRequestFullScreen()
+            } else if (document.documentElement.webkitRequestFullscreen) {
+                document.documentElement.webkitRequestFullscreen(
+                    Element.ALLOW_KEYBOARD_INPUT
+                )
+            }
+        } else {
+            if (document.cancelFullScreen) {
+                document.cancelFullScreen()
+            } else if (document.mozCancelFullScreen) {
+                document.mozCancelFullScreen()
+            } else if (document.webkitCancelFullScreen) {
+                document.webkitCancelFullScreen()
+            }
+        }
     }
-  }
-  return (
-    <React.Fragment>
-        <div className="navbar-header">
-          <div className="d-flex">
-            <div className="navbar-brand-box">
-              <Link to="/" className="logo logo-dark">
+
+    return (
+        <React.Fragment>
+            <div className="navbar-header">
+                <div className="d-flex">
+                    <div className="navbar-brand-box">
+                        <Link to="/" className="logo logo-dark">
                 <span className="logo-sm">
-                  <img src={logo} alt="" height="22" />
+                  <img src={logo} alt="" height="22"/>
                 </span>
-                <span className="logo-lg">
-                  <img src={logoDark} alt="" height="17" />
+                            <span className="logo-lg">
+                  <img src={logoDark} alt="" height="17"/>
                 </span>
-              </Link>
+                        </Link>
 
-              <Link to="/" className="logo logo-light">
+                        <Link to="/" className="logo logo-light">
                 <span className="logo-sm">
-                  <img src={logo} alt="" height="22" />
+                  <img src={logo} alt="" height="22"/>
                 </span>
-                <span className="logo-lg">
-                  <img src={logoLight} alt="" height="30" />
+                            <span className="logo-lg">
+                  <img src={logoLight} alt="" height="30"/>
                 </span>
-              </Link>
-            </div>
-
-            <button
-              type="button"
-              className="btn btn-sm px-3 font-size-16 d-lg-none header-item waves-effect waves-light"
-              data-toggle="collapse"
-              onClick={() => {
-                props.toggleLeftmenu(!props.leftMenu)
-              }}
-              data-target="#topnav-menu-content"
-            >
-              <i className="fa fa-fw fa-bars"/>
-            </button>
-
-            <form className="app-search d-none d-lg-block">
-              <div className="position-relative">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Buscar..."
-                />
-                <span className="uil-search"></span>
-              </div>
-            </form>
-          </div>
-
-          <div className="d-flex">
-            <div className="dropdown d-inline-block d-lg-none ms-2">
-              <button
-                type="button"
-                className="btn header-item noti-icon waves-effect"
-                id="page-header-search-dropdown"
-                onClick={() => setSearch(!isSearch)}
-              >
-                <i className="uil-search"></i>
-              </button>
-              <div
-                className={
-                  isSearch
-                    ? "dropdown-menu dropdown-menu-lg dropdown-menu-right p-0 show"
-                    : "dropdown-menu dropdown-menu-lg dropdown-menu-right p-0"
-                }
-                aria-labelledby="page-header-search-dropdown"
-              >
-                <form className="p-3">
-                  <div className="form-group m-0">
-                    <div className="input-group">
-                      <input
-                        type="text"
-                        className="form-control"
-                        placeholder={props.t("Search") + "..."}
-                        aria-label="Recipient's username"
-                      />
-                      <div className="input-group-append">
-                        <button className="btn btn-primary" type="submit">
-                          <i className="mdi mdi-magnify"/>
-                        </button>
-                      </div>
+                        </Link>
                     </div>
-                  </div>
-                </form>
-              </div>
-            </div>
 
-{/*            <LanguageDropdown />*/}
+                    <button
+                        type="button"
+                        className="btn btn-sm px-3 font-size-16 d-lg-none header-item waves-effect waves-light"
+                        data-toggle="collapse"
+                        onClick={() => {
+                            props.toggleLeftmenu(!props.leftMenu)
+                        }}
+                        data-target="#topnav-menu-content"
+                    >
+                        <i className="fa fa-fw fa-bars"/>
+                    </button>
 
-{/*            <Dropdown
+                    <GeneralSearch/>
+                </div>
+
+                <div className="d-flex">
+                    <div className="dropdown d-inline-block d-lg-none ms-2">
+                        <button
+                            type="button"
+                            className="btn header-item noti-icon waves-effect"
+                            id="page-header-search-dropdown"
+                            onClick={() => setSearch(!isSearch)}
+                        >
+                            <i className="uil-search"></i>
+                        </button>
+                        <div
+                            className={
+                                isSearch
+                                    ? "dropdown-menu dropdown-menu-lg dropdown-menu-right p-0 show"
+                                    : "dropdown-menu dropdown-menu-lg dropdown-menu-right p-0"
+                            }
+                            aria-labelledby="page-header-search-dropdown"
+                        >
+                            <form className="p-3">
+                                <div className="form-group m-0">
+                                    <div className="input-group">
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            placeholder={props.t("Search") + "..."}
+                                            aria-label="Recipient's username"
+                                        />
+                                        <div className="input-group-append">
+                                            <button className="btn btn-primary" type="submit">
+                                                <i className="mdi mdi-magnify"/>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+
+                    {/*            <LanguageDropdown />*/}
+
+                    {/*            <Dropdown
               className="d-none d-lg-inline-block ms-1"
               isOpen={socialDrp}
               toggle={() => {
@@ -209,24 +194,24 @@ const Header = props => {
               </DropdownMenu>
             </Dropdown>*/}
 
-            <div className="dropdown d-none d-lg-inline-block ms-1">
-              <button
-                type="button"
-                className="btn header-item noti-icon waves-effect"
-                onClick={() => {
-                  toggleFullscreen()
-                }}
-                data-toggle="fullscreen"
-              >
-                <i className="uil-minus-path"></i>
-              </button>
-            </div>
+                    <div className="dropdown d-none d-lg-inline-block ms-1">
+                        <button
+                            type="button"
+                            className="btn header-item noti-icon waves-effect"
+                            onClick={() => {
+                                toggleFullscreen()
+                            }}
+                            data-toggle="fullscreen"
+                        >
+                            <i className="uil-minus-path"></i>
+                        </button>
+                    </div>
 
-            <UsersSalesDropdown />
-            <NotificationDropdown />
-            <ProfileMenu />
+                    <UsersSalesDropdown/>
+                    <NotificationDropdown/>
+                    <ProfileMenu/>
 
-{/*            <div className="dropdown d-inline-block">
+                    {/*            <div className="dropdown d-inline-block">
               <button
                 onClick={() => {
                   props.showRightSidebarAction(!props.showRightSidebar)
@@ -237,26 +222,26 @@ const Header = props => {
                 <i className="uil-cog"></i>
               </button>
             </div>*/}
-          </div>
-        </div>
-    </React.Fragment>
-  )
+                </div>
+            </div>
+        </React.Fragment>
+    )
 }
 
 Header.propTypes = {
-  leftMenu: PropTypes.any,
-  showRightSidebar: PropTypes.any,
-  showRightSidebarAction: PropTypes.func,
-  t: PropTypes.any,
-  toggleLeftmenu: PropTypes.func
+    leftMenu: PropTypes.any,
+    showRightSidebar: PropTypes.any,
+    showRightSidebarAction: PropTypes.func,
+    t: PropTypes.any,
+    toggleLeftmenu: PropTypes.func
 }
 
 const mapStatetoProps = state => {
-  const { layoutType, showRightSidebar, leftMenu } = state.Layout
-  return { layoutType, showRightSidebar, leftMenu }
+    const {layoutType, showRightSidebar, leftMenu} = state.Layout
+    return {layoutType, showRightSidebar, leftMenu}
 }
 
 export default connect(mapStatetoProps, {
-  showRightSidebarAction,
-  toggleLeftmenu,
+    showRightSidebarAction,
+    toggleLeftmenu,
 })(withTranslation()(Header))
